9c1a27f0979aa1c72c045efccd21f41b334d1a05
PrachiKarle <prachikkarle@gmail.com>
2025-08-07 17:05:12 +0530

    Second Commit

---
+++ README.md
@@ -0,0 +1,35 @@
+Custom URL Shortener (Node.js)
+
+A simple URL shortener built with Node.js, HTML, and JavaScript.  
+This project allows users to shorten long URLs, and **logged-in users** can create **custom short URLs**.
+
+Features
+
+- User login
+- Shorten long URLs
+- Custom short URL codes for logged-in users
+- Redirection to original URL via short code
+- In-memory session and URL storage (no database)
+- File-based frontend (`home.html`, `customize.html`)
+
+Project Structure
+
+   shorten_url/
+     ├── public/
+     │ ├── home.html
+     │ ├── customize.html
+     │ ├── Login.html
+     ├── index.js
+     └── README.md
+
+
+How to Run
+
+1. Clone the repository
+
+   git clone https://github.com/yourusername/url-shortener.git
+   cd url-shortener
+
+2. Run the server
+
+   http://localhost:3000/
--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const querystring = require("querystring");
 
 //generate random url path
 function generateShortCode(length = 6) {
-  const chars ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+  const chars =
+    "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
   let code = "";
   for (let i = 0; i < length; i++) {
     code += chars.charAt(Math.floor(Math.random() * chars.length));
@@ -14,27 +15,52 @@ function generateShortCode(length = 6) {
   return code;
 }
 
-
 //map that url
 const urlMap = {};
-
-
+const session = {};
+const users = {
+  Prachi: "prachi@23",
+};
 
 var server = http.createServer((req, res) => {
-  
-    //load home page
+  //load home page
   if (req.url === "/") {
     const filePath = path.join(__dirname, "public", "home.html");
     fs.readFile(filePath, (err, content) => {
       res.writeHead(200, { "content-type": "text/html" });
       res.end(content);
     });
-  } 
-  
-  
+  }
+
+  //login pafe
+  else if (req.url == "/Login.html") {
+    const filePath = path.join(__dirname, "public", "Login.html");
+    fs.readFile(filePath, (err, content) => {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(content);
+    });
+  }
+
+  //login
+  else if (req.url === "/login" && req.method === "POST") {
+    let body = "";
+    req.on("data", (d) => (body += d.toString()));
+    req.on("end", () => {
+      const { username, password } = querystring.parse(body);
+      if (users[username] && users[username] === password) {
+        const sessionId = generateShortCode(16);
+        session[sessionId] = username;
+        res.writeHead(302, { Location: "/customize" });
+        res.end();
+      } else {
+        res.writeHead(401, { "Content-Type": "text/plain" });
+        res.end("Invalid credentials");
+      }
+    });
+  }
+
   //shorten logic
   else if (req.url === "/shorten" && req.method === "POST") {
-
     //access url input
     let longurl = "";
     req.on("data", (d) => {
@@ -47,17 +73,67 @@ var server = http.createServer((req, res) => {
 
       //generate short code;
       const shortCode = generateShortCode();
-      //store in object 
+      //store in object
       urlMap[shortCode] = longUrl;
-      
       //display to user
       res.writeHead(200, { "Content-Type": "text/html" });
       res.end(
-        `<a href=http://localhost:3000/${shortCode}>http://localhost:3000/${shortCode}</a> `
+        `<p>Short URL created:</p>
+        <a href=http://localhost:3000/${shortCode}>http://localhost:3000/${shortCode}</a> `
       );
     });
-  } 
-  
+  }
+
+  //customize page
+  else if (req.url === "/customize") {
+    const filePath = path.join(__dirname, "public", "customize.html");
+    fs.readFile(filePath, (err, content) => {
+      res.writeHead(200, { "content-type": "text/html" });
+      res.end(content);
+    });
+  }
+
+  //custome shorten
+  else if (req.url === "/custom_shorten" && req.method === "POST") {
+    let longurl = "";
+    req.on("data", (d) => (longurl += d.toString()));
+
+    req.on("end", () => {
+      const parsedData = querystring.parse(longurl);
+      const longUrl = parsedData.longUrl?.trim();
+      const customCode = parsedData.shortUrl?.trim();
+
+      if (!longUrl) {
+        res.writeHead(400, { "Content-Type": "text/plain" });
+        return res.end("Long URL is required.");
+      }
+
+      let shortCode;
+
+      if (customCode) {
+        if (urlMap[customCode]) {
+          res.writeHead(409, { "Content-Type": "text/plain" });
+          return res.end("Custom code already exists.");
+        }
+        shortCode = customCode;
+      } else {
+        do {
+          shortCode = generateShortCode();
+        } while (urlMap[shortCode]);
+      }
+
+      urlMap[shortCode] = longUrl;
+
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(`
+      <p>Short URL created:</p>
+      <a href="http://localhost:3000/${shortCode}">
+        http://localhost:3000/${shortCode}
+      </a>
+    `);
+    });
+  }
+
   //shorten url redirection
   else if (req.method === "GET") {
     const code = req.url.slice(1);
@@ -65,15 +141,12 @@ var server = http.createServer((req, res) => {
     if (urlMap[code]) {
       res.writeHead(302, { Location: urlMap[code] });
       res.end();
-    } 
-    
-    else {
+    } else {
       res.writeHead(404, { "Content-Type": "text/plain" });
       res.end("Short URL not found");
     }
   }
 });
-
 
 //server listen
 server.listen(3000, () => {
